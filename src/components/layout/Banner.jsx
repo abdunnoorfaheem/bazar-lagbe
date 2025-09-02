77b64e39cca3c44ec12d2110453ffa3e417b9080
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-09-02 22:16:09 +0600

    banner section complete

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -8,32 +8,51 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 const Banner = () => {
+  
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    appendDots: dots => (
+      <div>
+        <ul className="flex justify-center gap-2 mt-3">{dots}</ul>
+      </div>
+    ),
+    customPaging: ()=> (
+      <div
+        className="
+          w-3 h-3 
+          rounded-full 
+          bg-gray-500 
+          hover:bg-red-500 
+          transition-all
+          absolute bottom-[50px]
+        "
+      ></div>
+    )
   };
   return (
     <>
-      <Container>
+    <div className="border-t-2 border-[#1d1d1d50]">
+       <Container className={"my-5"}>
         <Flex className={"justify-between"}>
-          <div className="w-[30%]">
+          <div className="w-[30%]  border-r-2 border-[#1d1d1d50]">
             <ul className="text-[16px] font-poppins">
-              <li>Woman’s Fashion</li>
-              <li>Men’s Fashion</li>
-              <li>Electronics</li>
-              <li>Home & Lifestyle</li>
-              <li>Medicine</li>
-              <li>Sports & Outdoor</li>
-              <li>Baby’s & Toys</li>
-              <li>Groceries & Pets</li>
-              <li>Health & Beauty</li>
+              <li className="py-2">Woman’s Fashion</li>
+              <li className="py-2">Men’s Fashion</li>
+              <li className="py-2">Electronics</li>
+              <li className="py-2">Home & Lifestyle</li>
+              <li className="py-2">Medicine</li>
+              <li className="py-2">Sports & Outdoor</li>
+              <li className="py-2">Baby’s & Toys</li>
+              <li className="py-2">Groceries & Pets</li>
+              <li className="py-2">Health & Beauty</li>
             </ul>
           </div>
           <div className="w-[67%]">
-            <Slider {...settings}>
+            <Slider {...settings} className="">
               <div>
                 <Image imgSrc={BannerIMG} />
               </div>
@@ -50,6 +69,8 @@ const Banner = () => {
           </div>
         </Flex>
       </Container>
+    </div>
+     
     </>
   );
 };
